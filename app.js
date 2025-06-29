d691e25a2fe1b63ca51ed38d341eb9a41ca8c468
shaik irfan <130488964+shaikirfan78676@users.noreply.github.com>
2025-06-29 12:57:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/calculate', (req, res) => {
+    const num1 = parseFloat(req.body.num1);
+    const num2 = parseFloat(req.body.num2);
+    const operation = req.body.operation;
+    let result;
+
+    switch(operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            result = num1 / num2;
+            break;
+        default:
+            result = 'Invalid operation';
+    }
+    res.send(`Result: ${result}`);
+});
+
+app.listen(port, () => {
+    console.log(`Calculator app listening at http://localhost:${port}`);
+});
